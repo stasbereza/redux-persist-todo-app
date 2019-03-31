4441d6cdf7711b5b2ab79fb019a6742cc717d79e
Stanislav <stanislav.bereza@gmail.com>
2019-04-01 02:17:46 +0300

    deleted build folder from .giignore

---
+++ build/precache-manifest.a167f272e81cbaae1be1a6c8b10de5ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "efb1c89e8a144bb5d7d2",
+    "url": "/static/js/main.648f3143.chunk.js"
+  },
+  {
+    "revision": "16677ec02d58efdf2c46",
+    "url": "/static/js/2.29ea9c0d.chunk.js"
+  },
+  {
+    "revision": "efb1c89e8a144bb5d7d2",
+    "url": "/static/css/main.190ee71b.chunk.css"
+  },
+  {
+    "revision": "0854733eb3384a9d795519afc4ff698e",
+    "url": "/index.html"
+  }
+];
