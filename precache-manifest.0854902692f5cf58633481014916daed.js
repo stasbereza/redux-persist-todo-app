adff44e520608e129f754ed23136228da5fe3d34
Stanislav <stanislav.bereza@gmail.com>
2019-04-01 02:36:16 +0300

    Updates

---
+++ precache-manifest.0854902692f5cf58633481014916daed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dd232505147452548cc",
+    "url": "/redux-persist-todo-app/build/static/js/runtime~main.c8c68fb4.js"
+  },
+  {
+    "revision": "0449f7fe25f7e1d027cf",
+    "url": "/redux-persist-todo-app/build/static/js/main.757b8376.chunk.js"
+  },
+  {
+    "revision": "ec9921b7904d96b1f200",
+    "url": "/redux-persist-todo-app/build/static/js/2.1b35dce5.chunk.js"
+  },
+  {
+    "revision": "0449f7fe25f7e1d027cf",
+    "url": "/redux-persist-todo-app/build/static/css/main.190ee71b.chunk.css"
+  },
+  {
+    "revision": "2f0c55b787816d33965bdcca8f89a425",
+    "url": "/redux-persist-todo-app/build/index.html"
+  }
+];
